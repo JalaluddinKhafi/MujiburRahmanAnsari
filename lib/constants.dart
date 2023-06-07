6048ea657b3341705332696ccf0d8ae6705922cf
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-06-07 17:16:53 +0430

    Homepage first picture added

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+const FOOD_DATA = [
+  {
+    "name":"Burger",
+    "image":"assets/images/picture_5.png"
+  },{
+    "name":"Cheese Dip",
+    "image":"assets/images/picture_1.jpg"
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'Screens/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,91 +8,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Jalaluddin khafi'),
-        actions: [
-          Icon(CupertinoIcons.chat_bubble_text),
-          SizedBox(
-            width: 4,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 0, 16, 0),
-            child: Icon(CupertinoIcons.ellipsis_vertical),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 5, 20, 5),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.asset(
-                    'assets/images/picture_1.jpg',
-                    width: 70,
-                    height: 70,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  Text(
-                    'شهید مولانا مجیب الرحمن انصاری (رح)',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/picture_7.jpg',
-                      height: 100,
-                      width: 100,
-                    ),
-                    Expanded(
-                      child: Container(
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      'تفسیر قرآنکریم',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 5,),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+
