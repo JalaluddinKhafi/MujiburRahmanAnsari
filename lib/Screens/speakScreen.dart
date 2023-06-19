d57e1dcc30885497bfb1d33d5691769e615ddae0
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-06-19 22:53:39 +0430

    speakScreen rebuild

--- lib/Screens/speakScreen.dart
+++ lib/Screens/speakScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class spScreen extends StatefulWidget {
   const spScreen({Key? key}) : super(key: key);
@@ -10,13 +11,54 @@ class _spScreenState extends State<spScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      endDrawer: const Drawer(
-        child: Center(
-          child: Text("this is drawer"),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.teal
         ),
-      ),
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back_ios),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 70,left: 30,right: 30),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Icon(Icons.arrow_back_ios,size: 20,color: Colors.white,),
+                      Expanded(child: Container(),),
+                      Icon(Icons.info_outline,size: 20,color: Colors.white,),
+
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Text(
+                    "سخنرانی های شهید مولانا انصاری (رح)",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 5,),
+                  Text("تعداد سخنرانی های شهید مولانا انصاری (رح)در هر بخشی موجود است, ولی درین برنامه تعداد کمی از آنهارا ما آوردیم",
+                  textAlign:TextAlign.right,
+                  )
+                ],
+              ),
+            ),
+            Expanded(child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(70),
+                )
+
+              ),
+            ),
+            ),
+          ],
+        ),
       ),
     );
   }
