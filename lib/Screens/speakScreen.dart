7a6e608f409e2eb03531e266d0fc4550b9331f00
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-06-20 04:31:35 +0430

    url used and videos used from network

--- lib/Screens/speakScreen.dart
+++ lib/Screens/speakScreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:profile/Screens/HomePage.dart';
 import 'package:video_player/video_player.dart';
+
 class spScreen extends StatefulWidget {
   const spScreen({Key? key}) : super(key: key);
 
@@ -13,15 +14,15 @@ class spScreen extends StatefulWidget {
 
 class _spScreenState extends State<spScreen> {
   List videoInfo = [];
-  bool _playArea=false;
+  bool _playArea = false;
   late VideoPlayerController _controller;
   _initData() async {
     await DefaultAssetBundle.of(context)
         .loadString("json/videoInfo.json")
         .then((value) {
-     setState(() {
-       videoInfo = json.decode(value);
-     });
+      setState(() {
+        videoInfo = json.decode(value);
+      });
     });
   }
 
@@ -38,91 +39,99 @@ class _spScreenState extends State<spScreen> {
         decoration: BoxDecoration(color: Colors.teal),
         child: Column(
           children: [
-            _playArea==false?Container(
-              padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
-              width: MediaQuery.of(context).size.width,
-              height: 300,
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      InkWell(
-                          onTap: () {
-                            Get.back();
-                          },
-                          child: Icon(
-                            Icons.arrow_back_ios,
-                            size: 20,
-                            color: Colors.white,
-                          )),
-                      Expanded(
-                        child: Container(),
-                      ),
-                      Icon(
-                        Icons.info_outline,
-                        size: 20,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "سخنرانی های شهید مولانا انصاری (رح)",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 20,
+            _playArea == false
+                ? Container(
+                    padding:
+                        const EdgeInsets.only(top: 70, left: 30, right: 30),
+                    width: MediaQuery.of(context).size.width,
+                    height: 300,
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            InkWell(
+                                onTap: () {
+                                  Get.back();
+                                },
+                                child: Icon(
+                                  Icons.arrow_back_ios,
+                                  size: 20,
+                                  color: Colors.white,
+                                )),
+                            Expanded(
+                              child: Container(),
+                            ),
+                            Icon(
+                              Icons.info_outline,
+                              size: 20,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "سخنرانی های شهید مولانا انصاری (رح)",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 20,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "تعداد سخنرانی های شهید مولانا انصاری (رح)در هر بخشی موجود است, ولی درین برنامه تعداد کمی از آنهارا ما آوردیم",
+                          textAlign: TextAlign.right,
+                        )
+                      ],
+                    ),
+                  )
+                : Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          padding: const EdgeInsets.only(
+                              top: 50, left: 30, right: 30),
+                          child: Row(
+                            children: [
+                              InkWell(
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.arrow_back_ios,
+                                  size: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Expanded(child: Container()),
+                              Icon(Icons.info_outline,
+                                  size: 20, color: Colors.white)
+                            ],
+                          ),
+                        ),
+                        _playView(context),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    "تعداد سخنرانی های شهید مولانا انصاری (رح)در هر بخشی موجود است, ولی درین برنامه تعداد کمی از آنهارا ما آوردیم",
-                    textAlign: TextAlign.right,
-                  )
-                ],
-              ),
-            ):Container(
-             child: Column(
-               children: [
-                 Container(
-                   height: 100,
-                   padding: const EdgeInsets.only(top: 50,left: 30,right: 30),
-                   child: Row(
-                     children: [
-                       InkWell(
-                         onTap: (){
-                         },
-                         child: Icon(Icons.arrow_back_ios,size: 20,color: Colors.white,),
-                       ),
-                       Expanded(child: Container()),
-                       Icon(Icons.info_outline,size: 20,color: Colors.white)
-                     ],
-                   ),
-                 ),
-                 _playView(context),
-               ],
-             ),
-            ),
-            Expanded(child: Container(
+            Expanded(
+              child: Container(
                 padding: EdgeInsets.only(top: 20),
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(70),
-                    ),
-                ),
-              child: Column(
-                children: [
-                  Expanded(
-                      child: _listView(),
-                  
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(70),
                   ),
-                ],
-              ),
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: _listView(),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -131,27 +140,39 @@ class _spScreenState extends State<spScreen> {
     );
   }
 
-  _listView(){
+  _onTapVideo(int index) {
+    final controller = VideoPlayerController.network(videoInfo[index]["videoUrl"]);
+    _controller = controller;
+    setState(() {});
+    _controller
+      ..initialize().then((_) {
+        _controller.play();
+        setState(() {});
+      });
+  }
+
+  _listView() {
     return ListView.builder(
-      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 8),
-      itemCount:videoInfo.length,
-      itemBuilder: (_,int index){
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      itemCount: videoInfo.length,
+      itemBuilder: (_, int index) {
         return GestureDetector(
-          onTap: (){
+          onTap: () {
             _onTapVideo(index);
             debugPrint(index.toString());
             setState(() {
-              if(_playArea==false){
-                _playArea=true;
+              if (_playArea == false) {
+                _playArea = true;
               }
             });
           },
-          child: _buildCard(index) ,
+          child: _buildCard(index),
         );
-      },);
+      },
+    );
   }
 
-  _buildCard(int index){
+  _buildCard(int index) {
     return Container(
       height: 135,
       child: Column(
@@ -164,14 +185,13 @@ class _spScreenState extends State<spScreen> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   image: DecorationImage(
-                      image: AssetImage(
-                          videoInfo[index]["thumbnail"]
-                      ),
-                      fit: BoxFit.cover
-                  ),
+                      image: AssetImage(videoInfo[index]["thumbnail"]),
+                      fit: BoxFit.cover),
                 ),
               ),
-              SizedBox(width: 10,),
+              SizedBox(
+                width: 10,
+              ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -183,21 +203,23 @@ class _spScreenState extends State<spScreen> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 5,),
+                  SizedBox(
+                    height: 5,
+                  ),
                   Padding(
-                    padding: const EdgeInsets.only(top:3),
+                    padding: const EdgeInsets.only(top: 3),
                     child: Text(
                       videoInfo[index]["time"],
-                      style: TextStyle(
-                          color: Colors.grey[500]
-                      ),
+                      style: TextStyle(color: Colors.grey[500]),
                     ),
                   )
                 ],
               )
             ],
           ),
-          SizedBox(height: 18,),
+          SizedBox(
+            height: 18,
+          ),
           Row(
             children: [
               Container(
@@ -209,50 +231,51 @@ class _spScreenState extends State<spScreen> {
                 ),
                 child: Center(
                   child: Text(
-                    "15s rest",style: TextStyle(
-                    color: Color(0xFF839fed),
-                  ),
+                    "15s rest",
+                    style: TextStyle(
+                      color: Color(0xFF839fed),
+                    ),
                   ),
                 ),
               ),
               Row(
                 children: [
-                  for(int i=0;i<70;i++)
-                    i.isEven?Container(
-                      width: 3,
-                      height: 1,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF839fed),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ):Container(
-                      width: 3,
-                      height: 1,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                      ),
-                    )
+                  for (int i = 0; i < 70; i++)
+                    i.isEven
+                        ? Container(
+                            width: 3,
+                            height: 1,
+                            decoration: BoxDecoration(
+                              color: Color(0xFF839fed),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          )
+                        : Container(
+                            width: 3,
+                            height: 1,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                            ),
+                          )
                 ],
               )
             ],
           ),
-
         ],
       ),
     );
   }
 
-  _playView(BuildContext context){
-
-  }
-  _onTapVideo(int index){
-  final controller =VideoPlayerController.network(videoInfo[index]["videoUrl"]);
-    _controller=controller;
-    setState(() {
-
-    });
-    controller..initialize().then((_){
-      controller.play();
-    })
+  Widget _playView(BuildContext context) {
+    final controller = _controller;
+    if (controller != null && controller.value.isInitialized) {
+      return AspectRatio(
+        aspectRatio: 16 / 9,
+        child: VideoPlayer(controller),
+      );
+    } else {
+      return AspectRatio(
+          aspectRatio: 16 / 9, child: Text("Being initialized pls wait! "));
+    }
   }
 }
