70e3fbd5b85f33bd514440b144e8ec403c750a24
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-06-21 17:36:12 +0430

    achievement page created

--- lib/Screens/achievmentScreen.dart
+++ lib/Screens/achievmentScreen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class achievScreen extends StatelessWidget {
+class achievScreen extends StatefulWidget {
 
+  @override
+  State<achievScreen> createState() => _achievScreenState();
+}
+
+class _achievScreenState extends State<achievScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'آثار های شهید مولانا',
-        ),
+        title: Text('Jalaluddin khafi'),
         actions: [
           Icon(CupertinoIcons.chat_bubble_text),
           SizedBox(
@@ -21,9 +24,61 @@ class achievScreen extends StatelessWidget {
           ),
         ],
       ),
-      body:Column(
+      body: Column(
         children: [
-
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 5, 20, 5),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/picture_1.jpg',
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    'سه آثار از شهید مولانا انصاری (رح)',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Text(".............................................",),
+          SizedBox(height: 50,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/quran.jpg',
+                      height: 100,
+                      width: 100,
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'تفسیر قرآنکریم',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 5,),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
